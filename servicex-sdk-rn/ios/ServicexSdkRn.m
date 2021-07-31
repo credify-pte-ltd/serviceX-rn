40220e412ccd167bf798694f9a9082389a70841a
Danh Nguyen <dnncntt@gmail.com>
2021-07-31 18:35:19 +0700

    add serviceX ios sdk and its placehoder, remove unuse sample project

--- servicex-sdk-rn/ios/ServicexSdkRn.m
+++ servicex-sdk-rn/ios/ServicexSdkRn.m
@@ -2,9 +2,13 @@
 
 @interface RCT_EXTERN_MODULE(ServicexSdkRn, NSObject)
 
-RCT_EXTERN_METHOD(getOfferList:(NSString *)id
+RCT_EXTERN_METHOD(initialize:(NSString *)id environment:(NSString *)environment marketName:(NSString *)marketName)
+RCT_EXTERN_METHOD(getOfferList:(NSDictionary *)userDict
                  withResolver:(RCTPromiseResolveBlock)resolve
                  withRejecter:(RCTPromiseRejectBlock)reject)
+RCT_EXTERN_METHOD(clearCache)
+RCT_EXTERN_METHOD(showOfferDetail:(NSString *)offerId)
+RCT_EXTERN_METHOD(showReferral)
 
 @end
 
